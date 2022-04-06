226cf180ff44407d473361bfef0a057c71d6743e
Bangjay12345 <102670945+Bangjay12345@users.noreply.github.com>
2022-04-06 17:21:42 +0700

    Add files via upload

---
+++ Layout1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package layout;
+
+/**
+ *
+ * @author 62888
+ */
+public class Layout {
+ public static void main(String[] args) {
+        System.out.println("memmbuat layout kalkulator"); 
+    }
+}
